d4e374b3dad1ad724ac6ea7211257e9d6a46abdb
lixiaobin <lixiaobin8878@gmail.com>
2023-04-04 18:16:52 +0800

    fix: import nuxt.js

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  
+  <div>
+    lixiaobin
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
